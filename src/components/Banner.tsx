d486150ef3fe8d6890e7565ee8380269d1fe3446
Kristine Johnson <krisiowa01@gmail.com>
2026-02-07 21:58:27 -0800

    fixed conflicts

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 import React from "react";
 
-=======
->>>>>>> main
 interface BannerProps {
   onAboutClick: () => void;
 }
@@ -10,20 +7,12 @@ interface BannerProps {
 export default function Banner({ onAboutClick }: BannerProps) {
   return (
     <div className="banner">
-<<<<<<< HEAD
       <h1 className="kage-font">KageWolffoto</h1>
       <p>Capturing Moments, Creating Memories</p>
 
       <button className="about-link" onClick={onAboutClick}>
         About Me
       </button>
-=======
-      <h1>KageWolffoto</h1>
-
-      <a className="about-link" onClick={onAboutClick}>
-        About Me
-      </a>
->>>>>>> main
     </div>
   );
 }
