d0d3210097cbc1b9fcbd57826435d54ce1540431
Kristine Johnson <krisiowa01@gmail.com>
2026-02-07 21:59:03 -0800

    fixed conflicts (#38)

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 interface Photo {
   src: string;
   caption: string;
@@ -20,30 +19,6 @@ export default function Gallery({ photos }: GalleryProps) {
           </div>
         );
       })}
-=======
-import type { Photo } from "../types";
-
-interface GalleryProps {
-  albumName: string;
-  photos: Photo[];
-}
-
-export default function Gallery({ albumName: _albumName, photos }: GalleryProps) {
-  return (
-    <div className="gallery">
-      {photos.map((photo: Photo, index: number) => (
-        <div className="gallery-item" key={index}>
-          <img
-            src={photo.src}
-            alt={photo.caption}
-            className="watermarked"
-          />
-          <p className="gallery-caption" style={{ fontFamily: '"Playfair Display", serif' }}>
-            {photo.caption}
-          </p>
-        </div>
-      ))}
->>>>>>> main
     </div>
   );
 }
