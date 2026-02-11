919179f7cdc7cbdaafe0fd4b8f3c67357a6a67dc
Kristine Johnson <krisiowa01@gmail.com>
2026-02-10 19:01:21 -0800

    about me (#61)

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,11 +3,14 @@ export default function About() {
     <div className="about-section" id="about">
       <h2>KageWolf</h2>
       <p>
-       Hello! Thanks so much for visiting my photography portfolio. The story of how I started taking photos is… unconventional. I had a crush on a photographer and was hoping he’d notice me and maybe find me charming. Let’s just say it played out very differently in my head. But while the crush didn’t go anywhere, something else did: my connection with the camera. 
+       Hello! Thanks so much for visiting my photography portfolio. The story of how I started taking photos is… unconventional. 
+       <br />
+
+       <br />
+       I had a crush on a photographer and was hoping he’d notice me and maybe find me charming. Let’s just say it played out very differently in my head. But while the crush didn’t go anywhere, something else did: my connection with the camera. 
       
        That part was mutual. 
-       <br />
-       <br />
+    
 Heartbreak changes the way you move through the world, and for me, photography became the thing that helped me cope. It got me out of bed, out into the fresh air I desperately needed, and gave me something to focus on when everything felt heavy.
 
 My real start with the camera was in Cedar Rapids, IA. I felt comfortable wandering alone — past store windows, across bridges at night, and along quiet trails. I got to know my camera in those moments, and at the time, I thought my goal was to capture the city in a way that might convince someone I cared about to come back.
@@ -15,22 +18,20 @@ My real start with the camera was in Cedar Rapids, IA. I felt comfortable wander
 Looking back, I didn’t understand what they were going through, and I took their leaving way too personally. It hurt more than I expected. But that pain is what pushed me deeper into photography and helped me reconnect with myself and the place we once shared.       <br />
        <br />
        The opportunity to move to the PNW presented itself and as my heart pounded out of my chest, I prayed. Then, I moved. 
-       During this time the world was shut down due to COVID-19. Boy did that change my full experience living in the PNW but also opened up another opportunity. Working in an office - was not going to happen for a few more years, if not, ever again. I was fully remote.
+       During this time the world was shut down due to COVID-19. Boy did that change my full experience living in the PNW but also opened up another opportunity. Working in an office was not going to happen for a few more years, if not, ever again. I was fully remote.
        <br/>
-       The ONLY person I knew that no longer lived in Iowa and that was dear friend, lived in North Las Vegas, NV. I reached out to be thoughful of space and asked how she felt about me moving into the city. She was excited!! I prayed, then we moved.
+       The ONLY person I knew that no longer lived in Iowa and that was dear friend, lived in North Las Vegas, NV. I reached out to be thoughful of space and asked how she felt about me moving into the city. She was excited!! I prayed, then I moved.
         <br />
        <br />
        Las Vegas, NV !!! 
-       I’ve been in Las Vegas area for 5 years now and it’s been a wild ride. I’ve had the chance to explore the stunning landscapes of the Southwest, from the red rocks of Red Rock Canyon to the otherworldly beauty of Valley of Fire. I’ve also had the opportunity to connect with a vibrant community of photographers and artists who have inspired me to grow and push my creative boundaries. 
-       <br />
-       <br />
-       Photography has been a way for me to process emotions, capture moments, and connect with others in a way that words sometimes can’t. I’ve been fortunate to explore different styles and subjects, from landscapes to portraits, and everything in between. Each photo is a new adventure and a chance to see the world through a different lens.
-       <br />
-       <br />
+       I’ve been in Las Vegas area for 5 years now and I absolutely love the duality of this city. I’ve had the chance to explore the stunning landscapes of the Southwest, from the red rocks of Red Rock Canyon to the otherworldly beauty of Valley of Fire. I’ve also had the opportunity to connect with a vibrant community of photographers and artists who have inspired me to grow and push my creative boundaries. 
+       
 
-I live in Henderson, NV now, and I’m here for good. I love being outdoors and I’m always up for an adventure, but honestly, one‑day trips are my favorite — I’m a bit of a homebody at heart. I’m a natural introvert: a little shy, pretty private, and sometimes socially awkward. Photography has become the perfect outlet for me. It lets me express myself and connect with people without feeling overwhelmed. I get to stay behind the lens while still being part of the moment.
+I live in Henderson, NV now, and I’m here for good. I love being outdoors and I’m always up for an adventure, but honestly, one‑day trips are my favorite I’m quite a bit of a homebody. I’m a natural introvert: a little shy, pretty private, and sometimes socially awkward. Photography has become the perfect outlet for me. It lets me express myself and connect with people without feeling overwhelmed. I get to stay behind the lens while still being part of the moment.
 
-I’m also a huge tech nerd — completely obsessed with anything related to technology. Once I discovered programming, I fell in love with it and knew it was what I wanted to do long‑term. This website is a small dream come true, built during a handful of free evenings. I’m working on improving my frontend skills with React.js and Material UI, so if you have ideas for helpful features, feel free to email me and I’ll try them out.
+<br />
+<br />
+I’m a HUGE tech nerd, completely obsessed with anything related to technology. Once I discovered programming, I fell in love with it and knew it was what I wanted to do long‑term. This website is a small dream come true, built during a handful of free evenings. I’m working on improving my frontend skills with React.js and Material UI, so if you have ideas for helpful features, feel free to email me and I’ll try them out.
 
 If you ever want to talk photography or have questions, you can reach me at kage@kagewolffoto.com.
  </p>
