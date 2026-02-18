7f446776fa473a3f8924c5ec18b69f5a152681e7
Kristine Johnson <krisiowa01@gmail.com>
2026-02-17 19:14:22 -0800

    slider (#67)

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 interface BannerProps {
   onAboutClick: () => void;
 }
 
+const sliderImages = [
+  "/images/banner/DSC02493.webp",
+  "/images/banner/DSC02795.webp",
+  "/images/banner/DSC03693.webp"
+];
+
 export default function Banner({ onAboutClick }: BannerProps) {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prev) => (prev + 1) % sliderImages.length);
+    }, 5000); // change every 5 seconds
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="banner">
+    <div
+      className="banner slider-banner"
+      style={{ backgroundImage: `url(${sliderImages[index]})` }}
+    >
       <h1 className="kage-font">KageWolffoto</h1>
       <p>A Personal Journey Through Time and Space</p>
 
       <button className="about-link" onClick={onAboutClick}>
-        Why Photos?
+        About Me
       </button>
     </div>
   );
